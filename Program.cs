6ea99449326aee17105f72787641566c1c5c620f
EvS444 <emanuelsacoman@gmail.com>
2023-08-22 20:00:19 -0300

    Precisa de correção.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static int MDecRecursive(string s)
+    {
+        if (s.Length == 1)
+        {
+            Console.WriteLine($"Mdec(<{s}>) = {s}");
+            return Convert.ToInt32(s);
+        }
+        else
+        {
+            string prefix = s.Substring(0, s.Length - 1);
+            char lastDigit = s[s.Length - 1];
+
+            int subResult = MDecRecursive(prefix);
+            int result = 10 * subResult + Convert.ToInt32(lastDigit);
+
+            Console.WriteLine($"Mdec(<{prefix}> '{lastDigit}') = 10 * Mdec(<{prefix}>) + {lastDigit} = {result}");
+            return result;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        string number = "853";
+        int result = MDecRecursive(number);
+        Console.WriteLine($"Final result: {result}");
+    }
+}
