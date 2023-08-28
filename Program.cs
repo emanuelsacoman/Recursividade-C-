7667f9265e9530521e5677dcbdb07120307821ea
EvS444 <emanuelsacoman@gmail.com>
2023-08-28 20:12:10 -0300

    Envio código de recursividade em C#

--- Program.cs
+++ Program.cs
@@ -1,30 +1,24 @@
 ﻿using System;
 
-class Program
-{
-    static int MDecRecursive(string s)
-    {
-        if (s.Length == 1)
-        {
+class MainClass {
+    public static int MDecRecursive(string s) {
+        if (s.Length == 1) {
             Console.WriteLine($"Mdec(<{s}>) = {s}");
-            return Convert.ToInt32(s);
-        }
-        else
-        {
+            return int.Parse(s);
+        } else {
             string prefix = s.Substring(0, s.Length - 1);
             char lastDigit = s[s.Length - 1];
 
             int subResult = MDecRecursive(prefix);
-            int result = 10 * subResult + Convert.ToInt32(lastDigit);
+            int result = 10 * subResult + (int)char.GetNumericValue(lastDigit);
 
             Console.WriteLine($"Mdec(<{prefix}> '{lastDigit}') = 10 * Mdec(<{prefix}>) + {lastDigit} = {result}");
             return result;
         }
     }
 
-    static void Main(string[] args)
-    {
-        string number = "853";
+    public static void Main(string[] args) {
+        string number = "724822";
         int result = MDecRecursive(number);
         Console.WriteLine($"Final result: {result}");
     }
